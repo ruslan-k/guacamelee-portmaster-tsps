397120514361f203fdabb03dd14bdff8e24f5ca6
Ruslan Kurbanov <wkurbanov@gmail.com>
2026-09-15 21:44:46 +0400

    fix: use upstream default input helper mapping

--- portmaster/Guacamelee.sh
+++ portmaster/Guacamelee.sh
@@ -227,11 +227,13 @@ if [ "$BRIDGE" -eq 1 ]; then
   echo "gl4es_es=$LIBGL_ES gl=$LIBGL_GL notest=$LIBGL_NOTEST shrink=$LIBGL_SHRINK fbotex=$LIBGL_FBOFORCETEX fb=$LIBGL_FB diag=$GUACAMELEE_GL_DIAG fbo_lifecycle=$GUACAMELEE_FBO_LIFECYCLE xport_diag=$GUACAMELEE_XPORT_DIAG depth_only_read_none=$GUACAMELEE_DEPTH_ONLY_READ_NONE real_glerror=$GUACAMELEE_REAL_GLERROR khr_debug=$GUACAMELEE_KHR_DEBUG op_ring=$GUACAMELEE_OP_RING pixel_probe=$GUACAMELEE_PIXEL_PROBE zero_viewport=$GUACAMELEE_ZERO_VIEWPORT rb_zero_size=$GUACAMELEE_RB_ZERO_SIZE fbo_texture_fallback=$GUACAMELEE_FBO_TEXTURE_FALLBACK unify_depth_stencil=$GUACAMELEE_UNIFY_DEPTH_STENCIL rb_format_fix=$GUACAMELEE_RB_FORMAT_FIX frontend_rb_size_fix=$GUACAMELEE_FRONTEND_RB_SIZE_FIX hardext_fix=$GUACAMELEE_HARDEXT_FIX"
   if [ "${GUACAMELEE_LEGACY_INPUT:-1}" = "1" ]; then
     INPUT_HELPER="$GPTOKEYB"
+    echo "input_helper=$INPUT_HELPER (upstream-default-map)"
+    $INPUT_HELPER "game-bin" &
   else
     INPUT_HELPER="$GPTOKEYB2"
+    echo "input_helper=$INPUT_HELPER (ini-map)"
+    $INPUT_HELPER "game-bin" -c "$GAMEDIR/guacamelee.ini" &
   fi
-  echo "input_helper=$INPUT_HELPER"
-  $INPUT_HELPER "game-bin" -c "$GAMEDIR/guacamelee.ini" &
   pm_platform_helper "$GAMEDIR/box86/box86"
   if [ "$GUACAMELEE_HARDEXT_FIX" != "0" ]; then
     if [ "${GUACAMELEE_STRACE:-0}" != "0" ] && command -v strace >/dev/null 2>&1; then
@@ -256,11 +258,13 @@ else
   export LIBGL_SHRINK=4
   if [ "${GUACAMELEE_LEGACY_INPUT:-1}" = "1" ]; then
     INPUT_HELPER="$GPTOKEYB"
+    echo "input_helper=$INPUT_HELPER (upstream-default-map)"
+    $INPUT_HELPER "game-bin" &
   else
     INPUT_HELPER="$GPTOKEYB2"
+    echo "input_helper=$INPUT_HELPER (ini-map)"
+    $INPUT_HELPER "game-bin" -c "$GAMEDIR/guacamelee.ini" &
   fi
-  echo "input_helper=$INPUT_HELPER"
-  $INPUT_HELPER "game-bin" -c "$GAMEDIR/guacamelee.ini" &
   pm_platform_helper "$GAMEDIR/box86/box86"
   "$GAMEDIR/box86/box86" "$GAMEDIR/gamedata/game-bin"
   result=$?
